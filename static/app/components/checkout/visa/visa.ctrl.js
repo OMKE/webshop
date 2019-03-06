487d83586bfbcac8dd37fb2eab4da84706d7a24b
Omar Iriskic <contact@omaririskic.com>
2019-03-06 23:37:41 +0100

    payments

---
+++ static/app/components/checkout/visa/visa.ctrl.js
@@ -0,0 +1,17 @@
+(function (angular) {
+    angular.module("app").controller("VisaCtrl", ["$stateParams", "$http", "$state", "$rootScope", "$location", "$timeout", function ($stateParams, $http, $state, $rootScope, $location, $timeout) {
+        
+        let that = this;
+        this.host = "http://" + $location.host() + ":" + $location.port(); // dynamic for localhost, when in production delete port
+        
+        
+        
+        
+
+        
+        
+
+        
+
+    }]);
+})(angular);
